2c000c6e5412981831311786d41dc4a1796926b9
IlyasIsHere <ilyashk5@gmail.com>
2025-02-10 19:01:45 +0100

    Handle nested block comments + Add more lexer tests

--- syntax/lexer/lexer.go
+++ syntax/lexer/lexer.go
@@ -1,12 +1,12 @@
 package lexer
 
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "strconv"
-  "strings"
-  "unicode"
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 type TokenType int
@@ -37,7 +37,7 @@ const (
 
 	// Data types
 	STR_CONST
-  BOOL_CONST
+	BOOL_CONST
 	INT_CONST
 
 	// Identifiers
@@ -74,281 +74,291 @@ func (tt TokenType) String() string {
 
 // Token represents a lexical token with its type, value, and position
 type Token struct {
-  Type TokenType
-  Literal string
-  Line int
-  Column int
+	Type    TokenType
+	Literal string
+	Line    int
+	Column  int
 }
 
 // Lexer is the lexical analyzer
 type Lexer struct {
-  reader *bufio.Reader
-  line int
-  column int
-  char rune
+	reader *bufio.Reader
+	line   int
+	column int
+	char   rune
 }
 
 // NewLexer creates a new lexer from an io.Reader
 func NewLexer(reader io.Reader) *Lexer {
-  l := &Lexer{
-    reader: bufio.NewReader(reader),
-    line: 1,
-    column: 0,
-    char: ' ',
-  }
-  return l
+	l := &Lexer{
+		reader: bufio.NewReader(reader),
+		line:   1,
+		column: 0,
+		char:   ' ',
+	}
+	return l
 }
 
 // readChar reads the next character from the input
 func (l *Lexer) readChar() {
-  var err error
-  l.char, _, err = l.reader.ReadRune()
-  if err != nil {
-    l.char = 0 // EOF
-  }
+	var err error
+	l.char, _, err = l.reader.ReadRune()
+	if err != nil {
+		l.char = 0 // EOF
+	}
 
-  l.column++
-  if l.char == '\n' {
-    l.line++
-    l.column = 0
-  }
+	l.column++
+	if l.char == '\n' {
+		l.line++
+		l.column = 0
+	}
 }
 
 // peekChar returns the next character without advancing the stream
 func (l *Lexer) peekChar() rune {
-  char, _, err := l.reader.ReadRune()
-  if err != nil {
-    return 0
-  }
-  err = l.reader.UnreadRune()
-  return char
+	char, _, err := l.reader.ReadRune()
+	if err != nil {
+		return 0
+	}
+	err = l.reader.UnreadRune()
+	return char
 }
 
-// skipWhiteSpace skips whitespace characters 
+// skipWhiteSpace skips whitespace characters
 func (l *Lexer) skipWhiteSpace() {
-  for unicode.IsSpace(l.char) {
-    l.readChar()
-  }
+	for unicode.IsSpace(l.char) {
+		l.readChar()
+	}
 }
 
 func (l *Lexer) readNumber() string {
-  var sb strings.Builder
-  for unicode.IsDigit(l.char) {
-    sb.WriteRune(l.char)
-    l.readChar()
-  }
-  return sb.String()
+	var sb strings.Builder
+	for unicode.IsDigit(l.char) {
+		sb.WriteRune(l.char)
+		l.readChar()
+	}
+	return sb.String()
 }
 
 func isIdentifierStart(char rune) bool {
-  return unicode.IsLower(char) || unicode.IsUpper(char)
+	return unicode.IsLower(char) || unicode.IsUpper(char)
 }
 
 func isIdentifierPart(char rune) bool {
-  return isIdentifierStart(char) || unicode.IsDigit(char) || char == '_'
+	return isIdentifierStart(char) || unicode.IsDigit(char) || char == '_'
 }
 
 func (l *Lexer) readIdentifier() string {
-  var sb strings.Builder
-  for isIdentifierPart(l.char) {
-    sb.WriteRune(l.char)
-    l.readChar()
-  }
-  return sb.String()
+	var sb strings.Builder
+	for isIdentifierPart(l.char) {
+		sb.WriteRune(l.char)
+		l.readChar()
+	}
+	return sb.String()
 }
 
 func (l *Lexer) readString() (string, error) {
-  var sb strings.Builder
-  l.readChar() // reading the first character
-  for l.char != '"' {
-    if l.char == 0 {
-      return "", fmt.Errorf("EOF in string constant")
-    }
-    if l.char == '\n' {
-      return "", fmt.Errorf("Unterminated string constant")
-    }
+	var sb strings.Builder
+	l.readChar() // reading the first character
+	for l.char != '"' {
+		if l.char == 0 {
+			return "", fmt.Errorf("EOF in string constant")
+		}
+		if l.char == '\n' {
+			return "", fmt.Errorf("Unterminated string constant")
+		}
+		if l.char == '\x00' {
+			return "", fmt.Errorf("Null character (\\0) in string constant")
+		}
 
-    if l.char == '\\' {
-      l.readChar()
-      switch l.char {
-      case 'b':
-        sb.WriteRune('\b')
-      case 't':
-        sb.WriteRune('\t')
-      case 'n':
-        sb.WriteRune('\n')
-      case 'f':
-        sb.WriteRune('\f')
-      case '\\':
-        sb.WriteRune('\\')
-      case '"':
-        sb.WriteRune('"')
-      case '0':
-        return "", fmt.Errorf("Null character (\\0) in string constant")
-      default:
-        sb.WriteRune(l.char)
-      }
-    } else {
-      sb.WriteRune(l.char)
-    }
+		if l.char == '\\' {
+			l.readChar()
+			switch l.char {
+			case 'b':
+				sb.WriteRune('\b')
+			case 't':
+				sb.WriteRune('\t')
+			case 'n':
+				sb.WriteRune('\n')
+			case 'f':
+				sb.WriteRune('\f')
+			case '\\':
+				sb.WriteRune('\\')
+			case '"':
+				sb.WriteRune('"')
+			case '0':
+				return "", fmt.Errorf("Null character (\\0) in string constant")
+			default:
+				sb.WriteRune(l.char)
+			}
+		} else {
+			sb.WriteRune(l.char)
+		}
 
-    l.readChar()
-  }
+		l.readChar()
+	}
 
-  l.readChar()
-  return sb.String(), nil
+	l.readChar()
+	return sb.String(), nil
 }
 
 func (l *Lexer) NextToken() Token {
-  l.skipWhiteSpace()
+	l.skipWhiteSpace()
 
-  tok := Token{
-    Line: l.line,
-    Column: l.column,
-  }
-  
-  switch {
-  case l.char == 0:
-    tok.Type = EOF
-    tok.Literal = ""
-  case l.char == '(':
-    if l.peekChar() == '*' {
-      // this is a comment block
-      l.readChar()
-      l.readChar()
-      for !(l.char == '*' && l.peekChar() == ')') {
-        if l.char == 0 {
-          tok.Type = ERROR
-          tok.Literal = "Unterminated comment"
-          return tok
-        }
-        l.readChar()
-      }
-      l.readChar()
-      l.readChar()
-      return l.NextToken()
+	tok := Token{
+		Line:   l.line,
+		Column: l.column,
+	}
 
-    } else {
-      tok.Type = LPAREN
-      tok.Literal = "("
-      l.readChar()
-    }
+	switch {
+	case l.char == 0:
+		tok.Type = EOF
+		tok.Literal = ""
+	case l.char == '(':
+		if l.peekChar() == '*' {
+			// this is a comment block
+			l.readChar()
+			l.readChar()
+			openCount := 1 // count of openings (to handle nested comments)
+			// (*(*(*)
+			for openCount != 0 {
+				if l.char == 0 {
+					tok.Type = ERROR
+					tok.Literal = "Unterminated comment"
+					return tok
+				} else if l.char == '(' && l.peekChar() == '*' {
+					openCount++
+					l.readChar()
+				} else if l.char == '*' && l.peekChar() == ')' {
+					openCount--
+					l.readChar()
+				}
 
-  case l.char == ')':
-    tok.Type = RPAREN
-    tok.Literal = ")"
-    l.readChar()
-  case l.char == '{':
-    tok.Type = LBRACE
-    tok.Literal = "{"
-    l.readChar()
-case l.char == '}':
-    tok.Type = RBRACE
-    tok.Literal = "}"
-    l.readChar()
-  case l.char == ';':
-    tok.Type = SEMI
-    tok.Literal = ";"
-    l.readChar()
-  case l.char == ':':
-    tok.Type = COLON
-    tok.Literal = ":"
-    l.readChar()
-  case l.char == ',':
-    tok.Type = COMMA
-    tok.Literal = ","
-l.readChar()
-  case l.char == '+':
-    tok.Type = PLUS
-    tok.Literal = "+"
-    l.readChar()
-  case l.char == '*':
-    tok.Type = TIMES
-    tok.Literal = "*"
-    l.readChar()
-  case l.char == '*':
-    tok.Type = TIMES
-    tok.Literal = "*"
-    l.readChar()
+				l.readChar()
+			}
+			return l.NextToken()
 
-  case l.char == '-':
-    if l.peekChar() == '-' {
-      // this is an inline comment
-      for l.char != '\n' && l.char != 0 {
-        l.readChar()
-      }
-      return l.NextToken()
-    } else {
-      tok.Type = MINUS 
-      tok.Literal = "-"
-      l.readChar()
-    }
+		} else {
+			tok.Type = LPAREN
+			tok.Literal = "("
+			l.readChar()
+		}
 
-  case l.char == '~': 
-    tok.Type = NEG
-    tok.Literal = "~"
-    l.readChar()
-  case l.char == '.':
-    tok.Type = DOT
-    tok.Literal = "."
-    l.readChar()
-  case l.char == '/':
-    tok.Type = DIVIDE
-    tok.Literal = "/"
-    l.readChar()
+	case l.char == ')':
+		tok.Type = RPAREN
+		tok.Literal = ")"
+		l.readChar()
+	case l.char == '{':
+		tok.Type = LBRACE
+		tok.Literal = "{"
+		l.readChar()
+	case l.char == '}':
+		tok.Type = RBRACE
+		tok.Literal = "}"
+		l.readChar()
+	case l.char == ';':
+		tok.Type = SEMI
+		tok.Literal = ";"
+		l.readChar()
+	case l.char == ':':
+		tok.Type = COLON
+		tok.Literal = ":"
+		l.readChar()
+	case l.char == ',':
+		tok.Type = COMMA
+		tok.Literal = ","
+		l.readChar()
+	case l.char == '+':
+		tok.Type = PLUS
+		tok.Literal = "+"
+		l.readChar()
+	case l.char == '*':
+		tok.Type = TIMES
+		tok.Literal = "*"
+		l.readChar()
+	case l.char == '*':
+		tok.Type = TIMES
+		tok.Literal = "*"
+		l.readChar()
 
-  case l.char == '=':
-    if l.peekChar() == '>' {
-      tok.Type = DARROW
-      tok.Literal = "=>"
-      l.readChar()
-      l.readChar()
-    } else {
-      tok.Type = EQ
-      tok.Literal = "="
-      l.readChar()
-    }
+	case l.char == '-':
+		if l.peekChar() == '-' {
+			// this is an inline comment
+			for l.char != '\n' && l.char != 0 {
+				l.readChar()
+			}
+			return l.NextToken()
+		} else {
+			tok.Type = MINUS
+			tok.Literal = "-"
+			l.readChar()
+		}
 
-  case l.char == '<':
-    if l.peekChar() == '=' {
-      tok.Type = LE
-      tok.Literal = "<="
-      l.readChar()
-      l.readChar()
-    } else if l.peekChar() == '-' {
-      tok.Type = ASSIGN 
-      tok.Literal = "<-"
-      l.readChar()
-      l.readChar()
-    } else {
-      tok.Type = LT
-      tok.Literal = "<" 
-      l.readChar()
-    }
-  case l.char == '"':
-    str, err := l.readString()
-    if err != nil {
-      tok.Type = ERROR
-      tok.Literal = err.Error()
-    } else {
-      tok.Type = STR_CONST
-      tok.Literal = str
-    }
-  case unicode.IsDigit(l.char):
-    num := l.readNumber()
-    if _, err := strconv.Atoi(num); err != nil {
-      // number too big
-      tok.Type = ERROR
-      tok.Literal = "Number out of range"
-    } else {
-      tok.Type = INT_CONST
-      tok.Literal = num
-    }
-  case isIdentifierStart(l.char):
-    identifier := l.readIdentifier()
-    tok.Literal = identifier
-    switch strings.ToLower(identifier) {
-    // Handle keywords
+	case l.char == '~':
+		tok.Type = NEG
+		tok.Literal = "~"
+		l.readChar()
+	case l.char == '.':
+		tok.Type = DOT
+		tok.Literal = "."
+		l.readChar()
+	case l.char == '/':
+		tok.Type = DIVIDE
+		tok.Literal = "/"
+		l.readChar()
+
+	case l.char == '=':
+		if l.peekChar() == '>' {
+			tok.Type = DARROW
+			tok.Literal = "=>"
+			l.readChar()
+			l.readChar()
+		} else {
+			tok.Type = EQ
+			tok.Literal = "="
+			l.readChar()
+		}
+
+	case l.char == '<':
+		if l.peekChar() == '=' {
+			tok.Type = LE
+			tok.Literal = "<="
+			l.readChar()
+			l.readChar()
+		} else if l.peekChar() == '-' {
+			tok.Type = ASSIGN
+			tok.Literal = "<-"
+			l.readChar()
+			l.readChar()
+		} else {
+			tok.Type = LT
+			tok.Literal = "<"
+			l.readChar()
+		}
+	case l.char == '"':
+		str, err := l.readString()
+		if err != nil {
+			tok.Type = ERROR
+			tok.Literal = err.Error()
+		} else {
+			tok.Type = STR_CONST
+			tok.Literal = str
+		}
+	case unicode.IsDigit(l.char):
+		num := l.readNumber()
+		if _, err := strconv.Atoi(num); err != nil {
+			// number too big
+			tok.Type = ERROR
+			tok.Literal = "Number out of range"
+		} else {
+			tok.Type = INT_CONST
+			tok.Literal = num
+		}
+	case isIdentifierStart(l.char):
+		identifier := l.readIdentifier()
+		tok.Literal = identifier
+		switch strings.ToLower(identifier) {
+		// Handle keywords
 		case "class":
 			tok.Type = CLASS
 		case "if":
@@ -383,30 +393,24 @@ l.readChar()
 			tok.Type = NEW
 		case "not":
 			tok.Type = NOT
-    case "true", "false":
-      if unicode.IsLower(rune(identifier[0])) {
-        tok.Type = BOOL_CONST  
-      } else {
-        tok.Type = TYPEID
-      }
-    default:
-      if unicode.IsLower(rune(identifier[0])) {
-        tok.Type = OBJECTID
-      } else {
-        tok.Type = TYPEID
-      }
-    }
-  default:
-    tok.Type = ERROR
-    tok.Literal = fmt.Sprintf("Unexpected character: %c", l.char)
-    l.readChar()
-  }
+		case "true", "false":
+			if unicode.IsLower(rune(identifier[0])) {
+				tok.Type = BOOL_CONST
+			} else {
+				tok.Type = TYPEID
+			}
+		default:
+			if unicode.IsLower(rune(identifier[0])) {
+				tok.Type = OBJECTID
+			} else {
+				tok.Type = TYPEID
+			}
+		}
+	default:
+		tok.Type = ERROR
+		tok.Literal = fmt.Sprintf("Unexpected character: %c", l.char)
+		l.readChar()
+	}
 
-  return tok
+	return tok
 }
-
-
-
-
-
-
--- syntax/lexer/lexer_test.go
+++ syntax/lexer/lexer_test.go
@@ -46,6 +46,124 @@ func TestNextToken(t *testing.T) {
 			[]TokenType{CASE, OBJECTID, OF, OBJECTID, COLON, TYPEID, DARROW, BOOL_CONST, ESAC, EOF},
 			[]string{"case", "a", "of", "b", ":", "B", "=>", "false", "esac", ""},
 		},
+		{
+			"class List inherits Object { };",
+			[]TokenType{CLASS, TYPEID, INHERITS, TYPEID, LBRACE, RBRACE, SEMI, EOF},
+			[]string{"class", "List", "inherits", "Object", "{", "}", ";", ""},
+		},
+		{
+			"method(x : Int, y : String) : Bool { true };",
+			[]TokenType{OBJECTID, LPAREN, OBJECTID, COLON, TYPEID, COMMA, OBJECTID, COLON, TYPEID, RPAREN, COLON, TYPEID, LBRACE, BOOL_CONST, RBRACE, SEMI, EOF},
+			[]string{"method", "(", "x", ":", "Int", ",", "y", ":", "String", ")", ":", "Bool", "{", "true", "}", ";", ""},
+		},
+		{
+			"while (*(* nested comment *)*) not isvoid x loop\n pool",
+			[]TokenType{WHILE, NOT, ISVOID, OBJECTID, LOOP, POOL, EOF},
+			[]string{"while", "not", "isvoid", "x", "loop", "pool", ""},
+		},
+		{
+			"\"Hello\\tWorld\\n\"",
+			[]TokenType{STR_CONST, EOF},
+			[]string{"Hello\tWorld\n", ""},
+		},
+		{
+			"if x < y then x <- y else y <- x fi",
+			[]TokenType{IF, OBJECTID, LT, OBJECTID, THEN, OBJECTID, ASSIGN, OBJECTID, ELSE, OBJECTID, ASSIGN, OBJECTID, FI, EOF},
+			[]string{"if", "x", "<", "y", "then", "x", "<-", "y", "else", "y", "<-", "x", "fi", ""},
+		},
+		{
+			"1 + 2 * 3 / 4 - ~5",
+			[]TokenType{INT_CONST, PLUS, INT_CONST, TIMES, INT_CONST, DIVIDE, INT_CONST, MINUS, NEG, INT_CONST, EOF},
+			[]string{"1", "+", "2", "*", "3", "/", "4", "-", "~", "5", ""},
+		},
+		{
+			"new Object.method()",
+			[]TokenType{NEW, TYPEID, DOT, OBJECTID, LPAREN, RPAREN, EOF},
+			[]string{"new", "Object", ".", "method", "(", ")", ""},
+		},
+		{
+			"case x of\n y:Int => 1;\n z:String => \"hello\";\n esac",
+			[]TokenType{CASE, OBJECTID, OF, OBJECTID, COLON, TYPEID, DARROW, INT_CONST, SEMI, OBJECTID, COLON, TYPEID, DARROW, STR_CONST, SEMI, ESAC, EOF},
+			[]string{"case", "x", "of", "y", ":", "Int", "=>", "1", ";", "z", ":", "String", "=>", "hello", ";", "esac", ""},
+		},
+		{
+			"x -- comment 1\n<- -- comment 2\ny -- comment 3\n",
+			[]TokenType{OBJECTID, ASSIGN, OBJECTID, EOF},
+			[]string{"x", "<-", "y", ""},
+		},
+		{
+			"(* outer (* inner *) comment *) class",
+			[]TokenType{CLASS, EOF},
+			[]string{"class", ""},
+		},
+		{
+			"x <= y = z < w",
+			[]TokenType{OBJECTID, LE, OBJECTID, EQ, OBJECTID, LT, OBJECTID, EOF},
+			[]string{"x", "<=", "y", "=", "z", "<", "w", ""},
+		},
+		{
+			"(*(*(*(**)*)*)*)class",
+			[]TokenType{CLASS, EOF},
+			[]string{"class", ""},
+		},
+		{
+			"(*(*(*(*(*(*(*(*hello*)*)*)*)*)*)*)*) world",
+			[]TokenType{OBJECTID, EOF},
+			[]string{"world", ""},
+		},
+		{
+			"(* (* (* crazy (* nested (* comments *) here *) *) *) *) test",
+			[]TokenType{OBJECTID, EOF},
+			[]string{"test", ""},
+		},
+		{
+			"\"This is an unterminated string...",
+			[]TokenType{ERROR, EOF},
+			[]string{"EOF in string constant", ""},
+		},
+
+		// Test EOF Inside Multi-Line Comment
+		{
+			"(* This comment never ends...",
+			[]TokenType{ERROR, EOF},
+			[]string{"Unterminated comment", ""},
+		},
+		{
+			"\"Valid escape: \\x\"",
+			[]TokenType{STR_CONST, EOF},
+			[]string{"Valid escape: x", ""},
+		},
+		{
+			"    \n\t   ",
+			[]TokenType{EOF},
+			[]string{""},
+		},
+		{
+			"foo_bar123 <- 42;",
+			[]TokenType{OBJECTID, ASSIGN, INT_CONST, SEMI, EOF},
+			[]string{"foo_bar123", "<-", "42", ";", ""},
+		},
+		{
+			"If x Then y Else z Fi",
+			[]TokenType{IF, OBJECTID, THEN, OBJECTID, ELSE, OBJECTID, FI, EOF},
+			[]string{"If", "x", "Then", "y", "Else", "z", "Fi", ""},
+		},
+		{
+			"{ ( [ ] ) }",
+			[]TokenType{LBRACE, LPAREN, ERROR, ERROR, RPAREN, RBRACE, EOF},
+			[]string{"{", "(", "Unexpected character: [", "Unexpected character: ]", ")", "}", ""},
+		},
+		{
+			"12345678901234567890",
+			[]TokenType{ERROR, EOF},
+			[]string{"Number out of range", ""},
+		},
+
+		{
+			"a <- b;\n-- Comment line\nc <- d;",
+			[]TokenType{OBJECTID, ASSIGN, OBJECTID, SEMI, OBJECTID, ASSIGN, OBJECTID, SEMI, EOF},
+			[]string{"a", "<-", "b", ";", "c", "<-", "d", ";", ""},
+		},
 	}
 
 	for _, tt := range tests {
